a3bcd49553f4a1b83d168ddf6c596c030a08b122
Oren Farhi <farhioren@gmail.com>
2014-05-28 09:41:51 +0300

    added click test

--- testing/spec/ItemViewSpec.js
+++ testing/spec/ItemViewSpec.js
@@ -15,6 +15,12 @@ describe("ItemView - ", function() {
 		expect(view.$removeBtn).toHaveText('Remove');
 	});
 
+	it("should mark the item as done when clicked on the input", function(){
+		view.$input.click();
+		expect(model.isDone).toBeTruthy();
+		expect(view.$el).toHaveClass('done');
+	});
+
 	it("should have 'click' to check the item", function(){
 		var spyEvent = spyOnEvent(view.$input, 'click');
 		view.$input.click();
--- testing/spec/SpecHelper.js
+++ testing/spec/SpecHelper.js
@@ -1,17 +1,5 @@
 beforeEach(function () {
   jasmine.addMatchers({
-    toBePlaying: function () {
-      return {
-        compare: function (actual, expected) {
-          var player = actual;
-
-          return {
-            pass: player.currentlyPlayingSong === expected && player.isPlaying
-          }
-        }
-      };
-    },
-
     toBeDone: function() {
       return {
         compare: function(actual, expected) {
